e1113440efc0bf61d6a7ac355bce94ffc81f0dc2
raviroushan1818 <raviroushan1820@gmail.com>
2026-02-27 19:10:46 +0530

    done

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,11 @@ return Application::configure(basePath: dirname(__DIR__))
             'studio_owner' => \App\Http\Middleware\EnsureUserIsStudioOwner::class,
             'paid' => \App\Http\Middleware\EnsureCreatorOrBrandHasPaid::class,
         ]);
+        // PayU redirects here via POST without a CSRF token; we verify with PayU hash instead
+        $middleware->validateCsrfTokens(except: [
+            'payment/callback/success',
+            'payment/callback/failure',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
