f9b6452fb4d7fd16e65129e58eba2392051a611a
khushobb <khushboofarehawker1>
2026-02-28 17:21:51 +0530

    add campaign link in navbar

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -140,7 +140,7 @@
               </div>
             </Transition>
           </div>
-          <router-link to="/creators" class="text-sm font-semibold transition hover:text-[#fc4402]">Campaign</router-link>
+          <router-link to="/campaign-landing" class="text-sm font-semibold transition hover:text-[#fc4402]">Campaign</router-link>
 
                     <div
                         ref="servicesRef"
